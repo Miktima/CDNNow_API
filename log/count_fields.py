8d0b22cbcce450619eb681c7a3be7b4f63a7c6bb
Miktima <98176411+Miktima@users.noreply.github.com>
2022-12-20 17:37:50 +0300

    Update count_fields.py
    Add print result

--- log/count_fields.py
+++ log/count_fields.py
@@ -11,7 +11,7 @@ file_name = "user38610.39120bede8b01191ebcce1d729786f7d86364cd9fe7ea8320265cb449
 # Открываем файл, читаем заголовки и находим их положения
 log_table = pd.read_csv(file_name, ";")
 print (log_table.info())
-# print (log_table.groupby(field_count)[field_count].count())
+print((log_table[field_count].value_counts()).head(20))
 ((log_table[field_count].value_counts()).head(20)).plot()
 plt.show()
 
